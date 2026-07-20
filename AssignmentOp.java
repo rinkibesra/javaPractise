e3e7996294aa8acfbe889bccc124ab9d1bed039e
Rinki <rinkibesra.bca.aec@gmail.com>
2026-07-21 00:56:18 +0530

    operators

---
+++ AssignmentOp.java
@@ -0,0 +1,13 @@
+public class AssignmentOp {
+    public static void main(String[] args) {
+        int a=20;int b=30;int temp;
+        System.out.println("the value of a is "+a+" and value of b is "+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+    
+}

20baacf4ef68e777778fadc1c755f65ccbefd406
Rinki <rinkibesra.bca.aec@gmail.com>
2026-07-22 21:30:34 +0530

    while loop

--- bitwiseOddEven.java
+++ bitwiseOddEven.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 
 public class bitwiseOddEven {
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("To check wheater a number is odd / even");
-        System.out.print("Enter a number: ");
+        System.out.print
+        ("Enter a number: ");
         int num=input.nextInt();
         if((num&1)==1){
             System.out.println("Odd");
---
+++ whileLoop.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        // int num=1;
+        // while(num<=100){
+        //     System.out.print(num+" ");
+        //     num+=1;
+        // }
+        // int count=500;
+        // while(count>=200){
+        //     System.out.print(count+" ");
+        //     count-=1;
+        // }
+        Scanner input=new Scanner(System.in);
+        int i=1;
+        while(i<=5){
+            System.out.println("Enter a number");
+            int num=input.nextInt();            i++;
+
+        }
+    
+    
+}
+}
